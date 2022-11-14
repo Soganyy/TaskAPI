7429a5adfd5d01bb7615a327ceda50d8b0799513
Soganyy <matinmammadli0420@gmail.com>
2022-11-14 22:38:17 +0400

    Project done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let firstSectionCurrency = "";
-let secondSectionCurrency = "";
+let firstSectionCurrency = 0;
+let secondSectionCurrency = 0;
+let firstBtnText = "";
+let secondBtnText = "";
+let fs = 0;
+let ss = 0;
 
 const fsFirstBtn = document.querySelector('.firstRub');
 const fsSecondBtn = document.querySelector('.firstUsd');
@@ -7,29 +11,127 @@ const fsThirdBtn = document.querySelector('.firstEur');
 const fsFourthBtn = document.querySelector('.firstGbp');
 
 fsFirstBtn.addEventListener('click', () => {
-    fsFirstBtn.style = "background: #833AE0;" +
-    "color: #ffffff;";
-    fsSecondBtn.style = "background: white;" +
-    "color: #9F9F9F;";
+    fs = 1
+    fsFirstBtn.style = "background: #833AE0;" + "color: white;";
+    fsSecondBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsThirdBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsFourthBtn.style = "background: white;" + "color: #9F9F9F;";
 })
-
 fsSecondBtn.addEventListener('click', () => {
-    fsSecondBtn.style = "background: #833AE0;" +
-    "color: #ffffff;";
-    fsFirstBtn.style = "background: #ffffff;" +
-    "color: #9F9F9F;";
+    fs = 2;
+    fsFirstBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsSecondBtn.style = "background: #833AE0;" + "color: white;";
+    fsThirdBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsFourthBtn.style = "background: white;" + "color: #9F9F9F;"; 
 })
-
-
+fsThirdBtn.addEventListener('click', () => {
+    fs = 3
+    fsFirstBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsSecondBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsThirdBtn.style = "background: #833AE0;" + "color: white;";
+    fsFourthBtn.style = "background: white;" + "color: #9F9F9F;";
+})
+fsFourthBtn.addEventListener('click', () => {
+    fs = 4
+    fsFirstBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsSecondBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsThirdBtn.style = "background: white;" + "color: #9F9F9F;";
+    fsFourthBtn.style = "background: #833AE0;" + "color: white;";
+})
 
 const ssFirstBtn = document.querySelector('.secondRub');
 const ssSecondBtn = document.querySelector('.secondUsd');
 const ssThirdBtn = document.querySelector('.secondEur');
 const ssFourthBtn = document.querySelector('.secondGbp');
 
+ssFirstBtn.addEventListener('click', () => {
+    ss = 1;
+    ssFirstBtn.style = "background: #833AE0;" + "color: white;";
+    ssSecondBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssThirdBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssFourthBtn.style = "background: white;" + "color: #9F9F9F;";
+})
+ssSecondBtn.addEventListener('click', () => {
+    ss = 2;
+    ssFirstBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssSecondBtn.style = "background: #833AE0;" + "color: white;";
+    ssThirdBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssFourthBtn.style = "background: white;" + "color: #9F9F9F;"; 
+})
+ssThirdBtn.addEventListener('click', () => {
+    ss = 3;
+    ssFirstBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssSecondBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssThirdBtn.style = "background: #833AE0;" + "color: white;";
+    ssFourthBtn.style = "background: white;" + "color: #9F9F9F;";
+})
+ssFourthBtn.addEventListener('click', () => {
+    ss = 4;
+    ssFirstBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssSecondBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssThirdBtn.style = "background: white;" + "color: #9F9F9F;";
+    ssFourthBtn.style = "background: #833AE0;" + "color: white;";
+})
 
+const fInput = document.querySelector('.firstInput')
+const sInput = document.querySelector('.secondInput')
 
+function firstChange() {
+    switch(fs){
+        case 1: firstBtnText = 'RUB'
+        break;
+        case 2: firstBtnText = 'USD'
+        break;
+        case 3: firstBtnText = 'EUR'
+        break;
+        case 4: firstBtnText = 'GBP'
+    }
+    switch(ss){
+        case 1: secondBtnText = 'RUB'
+        break;
+        case 2: secondBtnText = 'USD'
+        break;
+        case 3: secondBtnText = 'EUR'
+        break;
+        case 4: secondBtnText = 'GBP'
+    }
+    fetch(`https://api.exchangerate.host/latest?base=${firstBtnText}&symbols=${secondBtnText}`)
+    .then(res => res.json())
+    .then(data => {
+        firstSectionCurrency = data.rates[Object.keys(data.rates)[0]];
+    })
+    function valueSet() {
+        sInput.value = fInput.value * firstSectionCurrency;
+    }
+    setTimeout(valueSet, 100)
+}
 
-fetch(`https://api.exchangerate.host/latest?base=USD&symbols=RUB `)
-.then(res => res.json())
-.then(data => {console.log(data);})
+function secondChange() {
+    switch(fs){
+        case 1: firstBtnText = 'RUB'
+        break;
+        case 2: firstBtnText = 'USD'
+        break;
+        case 3: firstBtnText = 'EUR'
+        break;
+        case 4: firstBtnText = 'GBP'
+    }
+    switch(ss){
+        case 1: secondBtnText = 'RUB'
+        break;
+        case 2: secondBtnText = 'USD'
+        break;
+        case 3: secondBtnText = 'EUR'
+        break;
+        case 4: secondBtnText = 'GBP'
+    }
+    fetch(`https://api.exchangerate.host/latest?base=${secondBtnText}&symbols=${firstBtnText}`)
+    .then(res => res.json())
+    .then(data => {
+        secondSectionCurrency = data.rates[Object.keys(data.rates)[0]];
+    })
+    function valueSet() {
+        fInput.value = sInput.value * secondSectionCurrency;
+    }
+    setTimeout(valueSet, 100)
+}
